3e2497ceacf789123da7cd01920045c13c050c39
Gabriel <gbcostasant@gmail.com>
2026-03-18 23:22:18 -0300

    header done

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import { useRef, useState } from "react";
-import { Bio } from "./components/bio";
-import { Header } from "./components/header";
-import { Skills } from "./components/skills";
-import { Contact } from "./components/contact";
+import { Header } from "./components/Header";
+
 function App() {
-  const constraintRef = useRef<any>(null);
-  const [stars, setStars] = useState(0);
   return (
-    <div
-      className="h-screen w-screen max-h-screen bg-zinc-100 flex flex-col items-center
-            overflow-hidden select-none"
-      ref={constraintRef}
-    >
-      <Header stars={stars} />
-      <Bio constraintRef={constraintRef} setStars={setStars} />
-      <Skills constraintRef={constraintRef} />
-      <Contact constraintRef={constraintRef} />
+    <div className="relative min-h-screen min-w-screen bg-zinc-950">
+      <Header />
     </div>
   );
 }
